4940117f6cd9690d7723c117c8f1c5dca1fc1554
Pertoldi <antoine.pertoldi@gmail.com>
2021-03-31 14:29:49 +0200

    Update server.js
    1er ligne de commentaires

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const http = require('http'); //TODO: lors du déploiement il faut rediriger les pages vers https:// grâce à un certificat SSL/TSL et utiliser la constante https = require('https')
 const app = require('./app');
 
 // revoie un port valide sous forme de numéro ou chaine
@@ -24,14 +24,15 @@ const errorHandler = error => { //gère les erreurs qui sont ensuite enristrées
     const address = server.address();
     const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
     switch (error.code) { 
+            
         case 'EACCES':
         console.error(bind + ' requires elevated privileges.');
-        process.exit(1);
         break;
+            
         case 'EADDRINUSE':
         console.error(bind + ' is already in use.');
-        process.exit(1);
         break;
+            
         default:
         throw error;
     }
